f1c1e0495982b1d0ae6f24fe8af98f01204ec303
lzx0014 <lzx0014@auburn.edu>
2016-09-10 17:29:48 -0500

    angleString

--- SoftwareProcess/Navigation/prod/Angle.py
+++ SoftwareProcess/Navigation/prod/Angle.py
@@ -1,28 +1,33 @@
+'''
+    CA01 by LZX0014
+
+'''
+
 import math
 class Angle():
     def __init__(self):
         self.angle = 0       
-        #set to 0 degrees 0 minutes
+        # set to 0 degrees 0 minutes
     
     def setDegrees(self, degrees=None):
         if degrees is None:
             self.angle = 0.0
         else:
-            if (not(isinstance(degrees,(int,float)))):
+            if (not(isinstance(degrees, (int, float)))):
                 raise ValueError('Angle.setDegrees:  The input should be a integer or float')
             self.angle = float(degrees)
             while self.angle < 0:
                 self.angle = self.angle + 360
-            while self.angle >360:
+            while self.angle > 360:
                 self.angle = self.angle - 360         
         return self.angle
      
          
-    def setDegreesAndMinutes(self, degrees):
-        if (not(isinstance(degrees,str))):
+    def setDegreesAndMinutes(self, angleString):
+        if (not(isinstance(angleString, str))):
             raise ValueError('Angle.setDegreesAndMinutes:  The input should be a string with a format of "xdy.y"')
-        parts = degrees.partition ('d')
-        if (not(parts[1]=='d')):
+        parts = angleString.partition ('d')
+        if (not(parts[1] == 'd')):
             raise ValueError('Angle.setDegreesAndMinutes:  No d in the input')
         try:
             integpart = int(parts[0])
@@ -34,16 +39,16 @@ class Angle():
             raise ValueError ('Angle.setDegreesAndMinutes:  minutes must be integer or float')
         if (floatpart < 0):
             raise ValueError('Angle.setDegreesAndMinutes:  minutes must be positive')
-        floatpartcheck = round(floatpart,1)
+        floatpartcheck = round(floatpart, 1)
         if (not(floatpart == floatpartcheck)):
             raise ValueError('Angle.setDegreesAndMinutes:  minutes must only have one decimal place')
         if (integpart < 0):
-            self.angle = integpart - floatpartcheck/60
+            self.angle = integpart - floatpartcheck / 60
         else:
-            self.angle = integpart + floatpartcheck/60
+            self.angle = integpart + floatpartcheck / 60
         while self.angle < 0:
             self.angle = self.angle + 360
-        while self.angle >360:
+        while self.angle > 360:
             self.angle = self.angle - 360         
         return self.angle       
             
@@ -52,7 +57,7 @@ class Angle():
     
         
     def add(self, angle):
-        if (not(isinstance(angle,Angle))):
+        if (not(isinstance(angle, Angle))):
             raise ValueError('Angle.add:  The input should be an angle')
         self.angle = self.angle + angle.angle
         while self.angle > 360:
@@ -60,7 +65,7 @@ class Angle():
         return self.angle
         
     def subtract(self, angle):
-        if (not(isinstance(angle,Angle))):
+        if (not(isinstance(angle, Angle))):
             raise ValueError('Angle.subtract:  The input should be an angle')
         self.angle = self.angle - angle.angle
         while self.angle < 0:
@@ -68,7 +73,7 @@ class Angle():
         return self.angle
     
     def compare(self, angle):
-        if (not(isinstance(angle,Angle))):
+        if (not(isinstance(angle, Angle))):
             raise ValueError('Angle.compare:  The input should be an angle')
         if self.angle == angle.angle:
             return 0
@@ -81,10 +86,10 @@ class Angle():
     
     def getString(self):
         Integpart = int(math.floor(self.angle))
-        Floatpart = round((self.angle - Integpart)*60,1)
+        Floatpart = round((self.angle - Integpart) * 60, 1)
         self.string = str(Integpart) + 'd' + str(Floatpart) 
         return self.string
     
     
     def getDegrees(self):
-        return round(self.angle,1)
+        return round(self.angle, 1)
